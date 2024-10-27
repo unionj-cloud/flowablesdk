e22c336b7184afcd51c3f2f8ee4d1ce5d0339b5f
武斌 <wubinwill@gmail.com>
2024-10-28 07:27:40 +0800

    fix

--- process_instance/request.go
+++ process_instance/request.go
@@ -6,17 +6,18 @@ import (
 )
 
 type ListRequest struct {
-	common.ListCommonRequest        // order allow id,processDefinitionId,tenantId,processDefinitionKey
-	Id                       string `json:"id,omitempty"`
-	ProcessDefinitionKey     string `json:"processDefinitionKey,omitempty"`
-	ProcessDefinitionId      string `json:"processDefinitionId,omitempty"`
-	BusinessKey              string `json:"businessKey,omitempty"`
-	InvolvedUser             string `json:"involvedUser,omitempty"`
-	Suspended                *bool  `json:"suspended,omitempty"`
-	SuperProcessInstanceId   string `json:"superProcessInstanceId,omitempty"`
-	SubProcessInstanceId     string `json:"subProcessInstanceId,omitempty"`
-	ExcludeSubprocesses      *bool  `json:"excludeSubprocesses,omitempty"`
-	IncludeProcessVariables  *bool  `json:"includeProcessVariables,omitempty"`
+	common.ListCommonRequest          // order allow id,processDefinitionId,tenantId,processDefinitionKey
+	ProcessInstanceId        string   `json:"processInstanceId,omitempty"`
+	ProcessInstanceIds       []string `json:"processInstanceIds,omitempty"`
+	ProcessDefinitionKey     string   `json:"processDefinitionKey,omitempty"`
+	ProcessDefinitionId      string   `json:"processDefinitionId,omitempty"`
+	BusinessKey              string   `json:"businessKey,omitempty"`
+	InvolvedUser             string   `json:"involvedUser,omitempty"`
+	Suspended                *bool    `json:"suspended,omitempty"`
+	SuperProcessInstanceId   string   `json:"superProcessInstanceId,omitempty"`
+	SubProcessInstanceId     string   `json:"subProcessInstanceId,omitempty"`
+	ExcludeSubprocesses      *bool    `json:"excludeSubprocesses,omitempty"`
+	IncludeProcessVariables  *bool    `json:"includeProcessVariables,omitempty"`
 	common.WithTenant
 }
 
